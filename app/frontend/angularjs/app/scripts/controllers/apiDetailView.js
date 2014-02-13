91f3004c20f21f1a696c5c039f753f731d47762e
Josh Hanna <sohj@frontology.org>
2014-02-13 12:48:03 -0600

    changed service uri to point to actual server

--- app/frontend/angularjs/app/scripts/controllers/apiDetailView.js
+++ app/frontend/angularjs/app/scripts/controllers/apiDetailView.js
@@ -3,7 +3,7 @@
 angular.module('ncsoDemo')
     .controller('APIDetailView', function ($scope, $http, $location, $routeParams) {
       // TODO: extract the below url to a config file
-      var serviceURL = 'http://localhost:9000/';
+      var serviceURL = 'http://144.30.12.7:9000/';
       $scope.apiName = $routeParams.route;
       $scope.apiPrettyName = $routeParams.prettyName;
 
